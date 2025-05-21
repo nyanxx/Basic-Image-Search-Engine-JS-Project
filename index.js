409b80ea8b19459fc1d33777c57515fe70aa2049
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-05-21 10:49:20 +0530

    #1 - API Code Added

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+import { API_KEY } from "./apikey.js";
+
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-btn");
 const searchDispArea = document.getElementById("search-disp-area");
 const listMoreImages = document.getElementById("list-more");
 
-searchButton.addEventListener("click", function() {
-  searchDispArea.innerText = searchInput.value
+let keyword = "";
+let page = 1;
+
+async function getImages() {
+    try {
+        keyword = searchInput.value;
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}`; //&client_id=${API_KEY}
+        const response = await fetch(url, { headers: { Authorization: `Client-ID ${API_KEY}` } });
+        const data = await response.json();
+        // console.log(data.errors[0]);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+searchButton.addEventListener("click", function () {
+    getImages();
 });
 
+listMoreImages.addEventListener("click", function () {
+    page += 1;
+    console.log(page);
+});
